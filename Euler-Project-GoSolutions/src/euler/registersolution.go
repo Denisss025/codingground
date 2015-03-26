85a62f8976f61f79fe256d99a00f7ed37e272222
Denis <denisss025@gmail.com>
2015-03-26 11:45:32 +0300

    Update registersolution.go

--- Euler-Project-GoSolutions/src/euler/registersolution.go
+++ Euler-Project-GoSolutions/src/euler/registersolution.go
@@ -1,5 +1,7 @@
 package euler
 
+import "sort"
+
 type SolutionFunc func()(uint64,error)
 
 type solutionType struct {
@@ -7,12 +9,27 @@ type solutionType struct {
     Func SolutionFunc
 }
 
-var solutions []solutionType
+type solutionsList []solutionType
+
+var solutions solutionsList
+
+func (s solutionsList) Len() int {
+    return len(s)
+}
+
+func (s solutionsList) Swap(i, j int) {
+    s[i], s[j] = s[j], s[i]
+}
+
+func (s solutionsList) Less(i, j int) bool {
+    return s[i].Name < s[j].Name
+}
 
 func RegisterSolution(name string, solution SolutionFunc) {
     solutions = append(solutions, solutionType{name, solution})
+    sort.Sort(solutions)
 }
 
 func init() {
-    solutions = make([]solutionType, 0, 100)
+    solutions = make(solutionsList, 0, 100)
 }
