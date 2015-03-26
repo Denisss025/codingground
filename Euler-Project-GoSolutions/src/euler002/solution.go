cbcb411de5349391de14f28bfd7c2895275d03f2
Denis <denisss025@gmail.com>
2015-03-26 11:34:20 +0300

    Created by Coding Ground Client

---
+++ Euler-Project-GoSolutions/src/euler002/solution.go
@@ -0,0 +1,14 @@
+package euler002
+
+import (
+    "euler"
+    "fmt"
+)
+
+func Solution() (uint64, error) {
+    return 0, fmt.Errorf("%s", "Not implemented yet")
+}
+
+func init() {
+    euler.RegisterSolution("002", Solution)
+}
